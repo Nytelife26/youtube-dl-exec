03c115f484aad87d304b5bd8250a0cf268ad8bf5
Nytelife26 <xtylerjrx@gmail.com>
2021-07-23 00:46:04 +0100

    refactor: update constants

--- src/constants.js
+++ src/constants.js
@@ -1,33 +1,18 @@
 'use strict'
 
-const isUnix = require('is-unix')
-const path = require('path')
+const { join } = require('path')
 
-const PLATFORM_WIN = 'win32'
-const PLATFORM_UNIX = 'unix'
-
-const YOUTUBE_DL_HOST =
-  process.env.YOUTUBE_DL_HOST ||
-  'https://api.github.com/repos/ytdl-org/youtube-dl/releases?per_page=1'
-
-const YOUTUBE_DL_DIR =
-  process.env.YOUTUBE_DL_DIR || path.join(__dirname, '..', 'bin')
-
-const YOUTUBE_DL_PLATFORM =
-  process.env.YOUTUBE_DL_PLATFORM || isUnix(process.platform)
-    ? PLATFORM_UNIX
-    : PLATFORM_WIN
-
-const YOUTUBE_DL_FILENAME =
-  process.env.YOUTUBE_DL_FILENAME ||
-  `youtube-dl${YOUTUBE_DL_PLATFORM === PLATFORM_WIN ? '.exe' : ''}`
-
-const YOUTUBE_DL_PATH = path.join(YOUTUBE_DL_DIR, YOUTUBE_DL_FILENAME)
+const dir = process.env.YOUTUBE_DL_DIR || join(__dirname, '..', 'bin')
+const unix = (process.env.YOUTUBE_DL_PLATFORM || process.platform) !== 'win32'
+const fn = process.env.YOUTUBE_DL_FILENAME || `youtube-dl${unix ? '' : '.exe'}`
+const path = join(dir, fn)
 
 module.exports = {
-  YOUTUBE_DL_DIR,
-  YOUTUBE_DL_FILENAME,
-  YOUTUBE_DL_HOST,
-  YOUTUBE_DL_PATH,
-  YOUTUBE_DL_PLATFORM
+  dir,
+  host:
+    process.env.YOUTUBE_DL_HOST ||
+    'https://api.github.com/repos/ytdl-org/youtube-dl/releases?per_page=1',
+  unix,
+  fn,
+  path
 }
